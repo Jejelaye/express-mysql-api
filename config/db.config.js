94bdbd87a182b48642aee44e58fe3342ef03d8d0
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2021-09-23 14:29:37 +0100

    made changes to use the parameters of production database

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb"
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "b46999e6f17d24",
+    PASSWORD: "e39bd90d",
+    DB: "heroku_60bad616428199f"
   };
