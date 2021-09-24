4c7cd26ab9a400fa40444d6960f689aff81a3d71
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2021-09-24 14:38:58 +0100

    added proper function to handle findAll query

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -28,19 +28,14 @@ exports.create = (req, res) => {
   };
 
 // Retrieve all Customers from the database.
-exports.findOne = (req, res) => {
-    Customer.findById(req.params.customerId, (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found Customer with id ${req.params.customerId}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Error retrieving Customer with id " + req.params.customerId
-          });
-        }
-      } else res.send(data);
+exports.findAll = (req, res) => {
+    Customer.getAll((err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving customers."
+        });
+      else res.send(data);
     });
   };
 
